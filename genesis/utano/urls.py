ac080e00f22b586e59ee1d16d33cd1199e6f1c3b
HMilato <hofumane@unilurio.ac.mz>
2020-05-24 17:07:00 +0200

    pra Matibe

--- genesis/utano/urls.py
+++ genesis/utano/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='Utano_Home'),
+    path('falar/', views.Falar, name='Utano_Falar'),
 ]
--- genesis/utano/views.py
+++ genesis/utano/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import speech_recognition as sr
 
 # Create your views here.
 
 def Home(request):
     return render(request, 'utano/home.html')
     # return HttpResponse ('<h1> Pizza Home </h1>')
+
+def Falar(request):
+    return render(request, 'utano/home2.html')
